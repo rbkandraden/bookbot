13162731f9dc282e9956f97a2cfb97830d37484d
rbkandraden <rebeccanasicmentoho@gmail.com>
2025-08-06 18:18:32 -0300

    Count Characters

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-from stats import count_words
+from stats import count_words, count_characters
 
 def get_text(filepath): 
-    with open(filepath) as f:
-        file_contents = f.read()
+    with open(filepath) as f: 
+        file_contents = f.read() 
     return file_contents
 
 def main():
-    text = get_text('books/frankenstein.txt')
-    num_words = count_words(text)
-    print(f"{75767} words found in the document")
+    text = get_text('books/frankenstein.txt') 
+    num_words = count_words(text) 
+    print(f"{num_words} words found in the document")
+    character_counts = count_characters(text)
+    print(character_counts)
 
 main()
--- stats.py
+++ stats.py
@@ -1,4 +1,14 @@
-def count_words(text): 
-    words = text.split()
-    num_words = len(words)
+def count_words(text): # Conta o número de palavras no texto
+    words = text.split() # Divide o texto em palavras
+    num_words = len(words) # Conta quantas palavras tem
     return num_words
+
+def count_characters(text): # Conta a frequencia de cada caractere no texto
+    characters = {} # Dicionário
+    text = text.lower() # Converte para minúsculas
+    for character in text:
+        if character in characters:
+            characters[character] += 1
+        else:
+            characters[character] = 1
+    return characters
